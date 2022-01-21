d02a7fcc90f5125cebab2d7e6c03598411c6444b
Seth Baffuor <baffuorakoto137@gmail.com>
2022-01-21 22:22:42 +0000

    Added logos

--- src/app/layout/Header.js
+++ src/app/layout/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import UserProfile from "../components/UserProfile";
-
+import Logo from "../../assets/gps_logo_white.png";
 const HeaderStyles = styled.div`
   .uk-navbar-container:not(.uk-navbar-transparent) {
     background-color: var(--color-primary) !important;
@@ -36,7 +36,19 @@ const Header = () => {
             zIndex: 980,
           }}
         >
-          <div>Logo</div>
+          <div
+            style={{
+              height: "inherit",
+              width: "100px",
+              overflow: "hidden",
+            }}
+          >
+            <img
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              src={Logo}
+              alt=""
+            />
+          </div>
           <div className="search-container">
             <form>
               <div>
